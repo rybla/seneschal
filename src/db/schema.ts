a47943acb4d52a127a9a5b84c49847f4f2f4ce26
Henry Blanchette <henry@rybl.net>
2026-02-09 11:48:12 -0500

    before database schemas

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,19 +3,4 @@ import z from "zod";
 
 // -----------------------------------------------------------------------------
 
-export type InsertUser = typeof userTable.$inferInsert;
-export type SelectUser = typeof userTable.$inferSelect;
-
-export const userTable = sqliteTable("userTable", {
-    id: int().primaryKey({ autoIncrement: true }),
-    name: text().notNull().unique(),
-    age: int().notNull(),
-    email: text().notNull().unique(),
-});
-
-export const InsertUserSchema = z.object({
-    name: z.string(),
-    age: z.number(),
-    email: z.string(),
-});
-
+// TODO: define database schemas
