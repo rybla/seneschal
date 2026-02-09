166f6088dfb60959f63405b7182e62c09de4b031
Henry Blanchette <henry@rybl.net>
2026-02-09 11:12:17 -0500

    LINKUP_API_KEY

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,7 @@ import z from "zod";
 export const EnvSchema = z.object({
     DATABASE_URL: z.string(),
     GEMINI_API_KEY: z.string(),
+    LINKUP_API_KEY: z.string(),
     PORT: z.coerce.number(),
 })
 
